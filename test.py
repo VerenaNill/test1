52bcc7231a0fa614802db4ddaeefc31caee26292
CarolinDupper <147599560+CarolinDupper@users.noreply.github.com>
2023-12-09 19:39:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #hallo 
-a = int(input("Werte fuer a: "))
+a = int(input("Wert fuer a: "))
 b = int(input("Wert fuer b: "))
 c = int(input("Wert fuer c: "))
 d = int(input("Wert fuer d: ")) 
@@ -21,5 +21,5 @@ print("Ergebnis 4:", ergebnis_4)
 print("Ergebnis 5:", ergebnis_5)
 print("Ergebnis 6:", ergebnis_6)
 print("Ergebnis 7:", ergebnis_7)
-print("Ergebnise 8:", ergebnis_8)
+print("Ergebnis 8:", ergebnis_8)
 
